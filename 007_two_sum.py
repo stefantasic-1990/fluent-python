96ba3bc0c0e63522358f8c9f452bcd83501d4462
Stefan Tasic <stefantasic.2790@gmail.com>
2025-09-22 20:29:00 -0400

    two sum and merge two sorted lists

--- 007_two_sum.py
+++ 007_two_sum.py
@@ -12,14 +12,32 @@
 input = [1, 2, 7, 10]
 tgt = 9
 
-def solve(string: list[int], target: int) -> bool:
+def solve(num_list: list[int], target: int) -> bool:
     a = 0
     b = 0
-    for i in range(len(string) - 1):
-        for j in range(i + 1, len(string)):
-            if string[i] + string[j] == tgt:
+    for i in range(len(num_list) - 1):
+        for j in range(i + 1, len(num_list)):
+            if num_list[i] + num_list[j] == tgt:
                 return [i, j]
     return []
 
-solution = solve(input, tgt)
+# The above is the O(n^2) version.
+# The below is O(n), an improvement
+
+def solve2(num_list: list[int], target: int) -> bool:
+    seen = {} # You want a dict because you need to track the index
+
+    for i, num in enumerate(num_list):
+        complement = target - num
+        if complement in seen:
+            return [i, seen[complement]]
+        seen[num] = i
+    return []
+
+solution = solve2(input, tgt)
 print(solution)
+
+# This improvement reduces the time complexity because:
+# It capitalizes on storing accessed values for reference.
+# This is instead of looping forward through them again and again.
+# You are "looking backward" to what was already seen as you iterate.
