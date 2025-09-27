957dcf7634ad50468415055505308312cdf85d5c
Stefan Tasic <stefantasic.2790@gmail.com>
2025-09-27 10:56:33 -0400

    manual frequency count of chars re-do

--- algorithms/002_manual-frequency-count-of-chars.py
+++ algorithms/002_manual-frequency-count-of-chars.py
@@ -3,9 +3,16 @@
 # Given a string, count how many times each character appears.
 # Return a dictionary where keys are characters and values are frequencies.
 
-input = "banana"
+s = "banana"
 
-def solve(string: str) -> dict[str, int]:
+def solve1(s: str) -> dict[str, int]:
+    freqs = {}
+    for ch in s:
+        freqs[ch] = freqs.get(ch, 0) + 1
 
+    return freqs
 
-print(solve(nums))
+print(solve1(s))
+
+# Using .get(key, default) ensures that a dict key entry that doesn't exist is initialized.
+# Another way is to explicitly check if an explicit if block, but that requires two lines.
