f69d2eb5819ae894520da1143bcd82853a786187
Stefan Tasic <stefantasic.2790@gmail.com>
2025-09-27 21:29:45 -0400

    two sum problem

--- algorithms/007_two-sum.py
+++ algorithms/007_two-sum.py
@@ -1,18 +1,26 @@
 # 007: Two Sum
 #
-# Given an array of integers nums and an integer target,
-# return the indices of the two numbers such that they add up to the target.
+# Given a list of integers and an integer target:
+# return the indices of the two numbers in the list that add up to the target.
 # 
-# You may assume that each input has exactly one solution,
-# and you may not use the same element twice.
+# You may assume that each input has exactly one solution.
+# You may not use the same element twice.
 # 
 # Return the answer as a list of two indices.
 # The order does not matter.
 
-ints = [1, 2, 7, 10]
-tgt = 9
+ints = [1, 2, 10, 7, 4]
+target = 9
 
-def solve(ints: list[int], tgt: int) -> bool:
-    
+def solve(ints: list[int], target: int) -> list[int]:
+    ints_map = {}
 
-print(solve(ints, tgt))
+    for i, num in enumerate(ints):
+        complement = target - num
+        if complement in ints_map:
+            return [ints_map[complement], i]
+        ints_map[num] = i
+
+    raise ValueError("No two numbers in list sum to target.")
+
+print(solve(ints, target))
