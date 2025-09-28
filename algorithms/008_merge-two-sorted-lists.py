f89f69c627c4248eca4e660fc3478a8a04671294
Stefan Tasic <stefantasic.2790@gmail.com>
2025-09-27 21:53:05 -0400

    merge two sorted lists re-do

--- algorithms/008_merge-two-sorted-lists.py
+++ algorithms/008_merge-two-sorted-lists.py
@@ -1,26 +1,27 @@
 # 008: Merge Two Sorted Lists
 #
-# You are given two independently sorted integer lists, a and b.
+# You are given two sorted integer lists, a and b.
 # Merge them into a single sorted list and return the result.
 # Do not use any built-in sort functions or libraries.
 
-a = [1, 3, 5]
-b = [2, 4, 6, 8]
+list_a = [1, 3, 5]
+list_b = [2, 4, 6, 8]
 
 def solve(list_a: list[int], list_b: list[int]) -> list[int]:
-    c = []
+    result = []
     i = j = 0
+
     while i < len(list_a) and j < len(list_b):
         if list_a[i] <= list_b[j]:
-            c.append(list_a[i])
+            result.append(list_a[i])
             i += 1
-        elif list_a[i] > list_b[j]:
-            c.append(list_b[j])
+        else:
+            result.append(list_b[j])
             j += 1
-    c.extend(list_a[i:])
-    c.extend(list_b[j:])
 
-    return c
+    result.extend(list_a[i:])
+    result.extend(list_b[j:])
 
-solution = solve(a, b)
-print(solution)
+    return result
+
+print(solve(list_a, list_b))
