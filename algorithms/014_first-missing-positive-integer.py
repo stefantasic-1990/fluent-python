03751986936208cedf5f4d2ab1a1a3a4f99e70f2
Stefan Tasic <stefantasic.2790@gmail.com>
2025-10-01 19:20:46 -0400

    first missing positive integer algo re-do

--- algorithms/014_first-missing-positive-integer.py
+++ algorithms/014_first-missing-positive-integer.py
@@ -4,20 +4,20 @@
 # You must solve this in O(n) time and O(1) extra space (i.e., in-place).
 #
 # Do not use extra hash sets or dicts.
-# Do not sort.
+# Do not use any sort functions.
 
-nums = [1, 2, 0]
+nums = [5, 2, 0, 3, 1]
 
 def solve(nums: list[int]) -> int:
-    for i in nums:
-        while 0 < nums[i] < len(nums) and nums[i] != nums[nums[i] - 1]:
-            nums[i], nums[nums[i] - 1] = nums[nums[i] - 1], nums[i]
+    for i in range(len(nums)):
+        while 1 <= nums[i] <= len(nums) and nums[i] != nums[nums[i] - 1]:
+            j = nums[i] - 1
+            nums[i], nums[j] = nums[j], nums[i]
 
     for i in range(len(nums)):
         if nums[i] != i + 1:
             return i + 1
-        
+
     return len(nums) + 1
 
-solution = solve(nums=nums)
-print(solution)
+print(solve(nums=nums))
