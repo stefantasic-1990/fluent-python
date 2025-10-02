f0c27c4bf96e9f78cefadd20a3d3e6f12759da3e
Stefan Tasic <stefantasic.2790@gmail.com>
2025-10-01 20:02:29 -0400

    longest consecutive sequence algo re-do

--- algorithms/016_longest-consecutive-sequence.py
+++ algorithms/016_longest-consecutive-sequence.py
@@ -5,13 +5,24 @@
 # 
 # You must solve it in O(n) time.
 # No sorting allowed.
-# You must use O(n) time.
 # Do not use the same number twice.
 # Duplicates may exist, but they don’t count toward the sequence.
 
 nums = [100, 4, 200, 1, 3, 2]
 
 def solve(nums: list[int]) -> int:
+    nums_set = set(nums)
+    longest_seq = 0
 
+    for n in nums_set:
+        if n - 1 not in nums_set:
+            current_num = n
+            current_seq = 1
+            while current_num + 1 in nums_set:
+                current_seq += 1
+                current_num += 1
+            longest_seq = max(current_seq, longest_seq)
+
+    return longest_seq
 
 print(solve(nums))
