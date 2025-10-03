f6758f441ab261adfb023d00bfb1fdc2d955f30b
Stefan Tasic <stefantasic.2790@gmail.com>
2025-10-03 08:53:55 -0400

    longest repeating character replacement

---
+++ algorithms/017_longest-repeating-character-replacement.py
@@ -0,0 +1,28 @@
+# 017: Longest Repeating Character Replacement
+#
+# You are given a string s and an integer k.
+# You may replace at most k characters in the string so that all the characters in some substring are the same.
+# Return the length of the longest such substring you can create.
+
+s = "AABABBABBAB"
+k = 2
+
+def solve(s: str, k: int) -> int:
+    j = 0
+    freqs = {}
+    max_freq = 0
+    longest = 0
+
+    for i in range(len(s)):
+        freqs[s[i]] = freqs.get(s[i], 0) + 1
+        max_freq = max(freqs[s[i]], max_freq)
+
+        while (i - j + 1) - max_freq > k:
+            freqs[s[j]] = freqs.get(s[j], 0) - 1
+            j += 1
+
+        longest = max(i - j + 1, longest)
+
+    return longest
+
+print(solve(s, k))
